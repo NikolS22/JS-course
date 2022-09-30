4b955ce58ce387172a0fce3c6bd0daad145095dd
Olga S <nikolsekr@mail.ru>
2022-09-30 17:04:31 +0300

    Загрузка файлов

---
+++ Less1/index.js
@@ -0,0 +1,22 @@
+console.log('Hello world!')
+
+const ourPlanetName ='Земля';
+console.log(ourPlanetName);
+
+let timeUser = '12:00';
+console.log(timeUser);
+
+let countArticle=5;
+console.log(countArticle);
+
+let isCatch= true;
+console.log(isCatch);
+
+let surName = 'Samulkina';
+console.log(surName);
+
+let Name = 'Olga';
+console.log(Name);
+
+let middleName = 'Evgenevna';
+console.log(middleName);
